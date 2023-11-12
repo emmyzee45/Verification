e0250ae320db7bea00c52eb66987d7470160df2b
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2023-11-12 12:11:48 +0100

    change url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://ec2-54-84-80-103.compute-1.amazonaws.com:5000/api/",
+  baseURL: "http://ec2-54-84-80-103.compute-1.amazonaws.com/api/",
   withCredentials: true,
 });
