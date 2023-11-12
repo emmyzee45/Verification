e2b4d8a4d3801c284d3bdd4435dda552411d828d
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2023-11-12 13:14:25 +0100

    url change

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://ec2-54-84-80-103.compute-1.amazonaws.com/api/",
+  baseURL: "https://simver.net/api/",
   withCredentials: true,
 });
